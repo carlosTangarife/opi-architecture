44caf7d2f3ece67bdda2c07684b17795f7cf31e9
carlos.tangarife <cjt860826@gmail.com>
2022-05-07 01:06:58 -0500

    initial commit

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'opi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'admin';
+}
