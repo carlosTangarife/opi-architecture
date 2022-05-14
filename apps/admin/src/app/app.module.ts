ab7a3f1d0f1d413ec7a18140b0f4356bcedd6efa
carlosTangarife <cjt860826@gmail.com>
2022-05-13 21:36:05 -0500

    se quitan componente de bienvenida no necesario

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { ArchimedesModule } from './archimedes/archimedes.module';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, ArchimedesModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ArchimedesModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
