ef830d8ee3299d3fa714fafdc5579559d61678e6
carlosTangarife <cjt860826@gmail.com>
2022-05-13 22:03:51 -0500

    mai, header y footer fueron creados

--- libs/layout/src/lib/layout.module.ts
+++ libs/layout/src/lib/layout.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [FooterComponent, HeaderComponent, MainComponent],
 })
 export class LayoutModule {}
